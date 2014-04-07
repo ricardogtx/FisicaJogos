c490039e40fb7131c31e62495b311d00bf863b58
CaioIcy <caio.nardelli@hotmail.com>
2014-04-07 20:38:57 -0300

    Begin refactoring

--- trabalho1.py
+++ trabalho1.py
@@ -4,17 +4,16 @@ from math import sqrt
 import math
 
 #Input do programa
-vel=input('Escreva a velocidade:')
-graus = input('Angulo:')
+vel = input('!! Escreva a velocidade: ')
+graus = input('!! Escreva o angulo: ')
 angulo = math.radians(graus)
-vox=vel*abs(math.cos(angulo))
+vox = vel*abs(math.cos(angulo))
 
 
-def F(r, v):
+def VetorForca(r, v):
     rx, ry = r
     vx, vy = v
     V = sqrt(vx**2 + vy**2)
-    
     return (-K * V * vx, 10 * m - K * V * vy)
     
 # Constantes globais da simulacao
@@ -33,49 +32,46 @@ N = int(T / dt)
 K = 0.5 * rho * A * Cd
 rx, ry = r0
 vx, vy = v0    
-Fx, Fy = F((rx, ry), (vx, vy))
+Fx, Fy = VetorForca((rx, ry), (vx, vy))
 ax, ay = Fx /m, Fy / m
 
-largura, altura = 1000, 480	
-tela = pygame.display.set_mode((largura, altura))
-mario = pygame.image.load("bola.gif")
+larguraTela, alturaTela = 192*5, 108*5	
+tela = pygame.display.set_mode((larguraTela, alturaTela))
+bola = pygame.image.load("bola.gif")
 relogio = pygame.time.Clock()
-
 
 x, y = 0, 450
 
-
-
 pygame.init()
-tela.fill((255,255,255))
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()   
             raise SystemExit
+
     # Calcula as aceleracoes
-    Fx, Fy = F((rx, ry), (vx, vy))
+    Fx, Fy = VetorForca((rx, ry), (vx, vy))
     ax = Fx / m
     ay = Fy / m
 
     # Atualiza as velocidades
-    	
     vx = vx + ax * dt
     vy = vy + ay * dt
 
     x += vx
     y += vy
+
     if x < 0:
         vx = 0
-    if y < 0 or y > altura-30:
-	vy = (vy*(-0.98))
-	if y > altura-30:
-        	y = altura-30
+    if y < 0 or y > alturaTela-30:
+        vy = (vy*(-0.98))
+        if y > alturaTela-30:
+            y = alturaTela-30
         if y < 0:
-		y=0
+            y=0
                 
-    
-    tela.blit(mario, (x, y))
-    relogio.tick(1000/30.)
+    tela.fill((255,255,255))
+    tela.blit(bola, (x, y))
+    relogio.tick(1000/30.0)
     pygame.display.flip()
  
